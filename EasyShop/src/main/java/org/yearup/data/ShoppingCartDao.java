93f86e40ab9c3631815c73d59dc1ddac95294ac0
Sujan Banjara <banjara.suzan12@gmail.com>
2025-06-23 14:43:20 -0500

    Define interface methods for shopping cart operations

--- EasyShop/src/main/java/org/yearup/data/ShoppingCartDao.java
+++ EasyShop/src/main/java/org/yearup/data/ShoppingCartDao.java
@@ -5,5 +5,9 @@ import org.yearup.models.ShoppingCart;
 public interface ShoppingCartDao
 {
     ShoppingCart getByUserId(int userId);
+
+    void addProduct(int userId, int productId);
+    void updateQuantity(int userId, int productId, int quantity);
+    void deleteCart(int userId);
     // add additional method signatures here
 }
